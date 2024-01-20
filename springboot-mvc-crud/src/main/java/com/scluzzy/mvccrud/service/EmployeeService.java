fc7f2b7efcdc2d5986c9dd7c38d9b8430b07a884
Abdulrahman Syed <3004sar2001@gmail.com>
2024-01-20 21:00:35 +0300

    Add app

---
+++ springboot-mvc-crud/src/main/java/com/scluzzy/mvccrud/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.scluzzy.mvccrud.service;
+
+import com.scluzzy.mvccrud.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    void save(Employee theEmployee);
+
+    void deleteById(int theId);
+
+}
---
+++ springboot-mvc-crud/src/test/java/com/scluzzy/mvccrud/SpringbootThymeleafMvcCrudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scluzzy.mvccrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootThymeleafMvcCrudDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
